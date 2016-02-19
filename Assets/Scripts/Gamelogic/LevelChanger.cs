b3a244d7626a78d075a674e7454e128107b841a1
David Portisch <david.portisch@outlook.com>
2016-02-20 00:41:05 +0100

    Implemented level changing and a main game scene which is always present for the levels

---
+++ Assets/Scripts/Gamelogic/LevelChanger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+	public void Start()
+	{
+		if (SceneManager.sceneCount == 1)
+		{
+			int levelSceneIndex = gameObject.scene.buildIndex;
+
+			SceneManager.LoadScene("Scenes/Game", LoadSceneMode.Single);
+			SceneManager.LoadScene(levelSceneIndex, LoadSceneMode.Additive);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			NextLevel();
+		}
+	}
+
+	public void NextLevel()
+	{
+		
+		int currentScene = gameObject.scene.buildIndex;
+        int nextSceneIndex = currentScene + 1;
+		if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+		{
+			SceneManager.UnloadScene(currentScene);
+			SceneManager.LoadScene(nextSceneIndex, LoadSceneMode.Additive);
+		}
+	}
+}
---
+++ Assets/Scripts/Gamelogic/LevelInitializer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelInitializer : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+		if (nextScene < SceneManager.sceneCountInBuildSettings)
+		{
+			SceneManager.LoadScene(nextScene, LoadSceneMode.Additive);
+		}
+	}
+}
