7836d25f2203f65a2eba02631ca800ce85322770
Daeradas <Night.Macig@gmail.com>
2016-02-20 11:33:16 +0100

    Sheep1

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public delegate void lifeChange(GameObject go);
+	public static event lifeChange onBirth;
+	public static event lifeChange onDeath;
+
+	// Use this for initialization
+	void Start () {
+		if(onBirth != null)
+		{
+			onBirth(gameObject);
+		}
+	}
+
+	void OnDestroy()
+	{
+		if (onDeath != null)
+		{
+			onDeath(gameObject);
+		}
+	}
+}
