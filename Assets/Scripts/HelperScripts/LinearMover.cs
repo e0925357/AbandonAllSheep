1016387b8d9885dc9d2c2bb2d6598d9eebec7cb8
Stefan Zaufl <stefan.zaufl@hotmail.com>
2016-02-20 15:59:38 +0100

    Added code & prefabs for fireballs.

---
+++ Assets/Scripts/HelperScripts/LinearMover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinearMover : MonoBehaviour {
+
+	public Vector3 unitsPerSecond;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += unitsPerSecond * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+	public GameObject prefab2Spawn;
+	public Vector3 projectileOffset;
+
+	public float waitTime = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(spawnProjectile());
+	}
+
+	IEnumerator spawnProjectile()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(waitTime);
+
+			Instantiate(prefab2Spawn, transform.position + (transform.rotation * projectileOffset), transform.rotation);
+		}
+	}
+}
