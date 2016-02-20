b6459acaf0c7caadcaf87aa11d9df714f572bfed
David Portisch <david.portisch@outlook.com>
2016-02-20 10:31:30 +0100

    Added spawn logic and fixed bug that the levels were loaded twice

--- Assets/Scripts/Gamelogic/LevelChanger.cs
+++ Assets/Scripts/Gamelogic/LevelChanger.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour
 {
+	private PlayerRespawn playerRespawn;
+
+	private static readonly string PLAYER_TAG_NAME = "Player";
+
 	public void Start()
 	{
 		if (SceneManager.sceneCount == 1)
@@ -25,13 +29,32 @@ public class LevelChanger : MonoBehaviour
 
 	public void NextLevel()
 	{
-		
 		int currentScene = gameObject.scene.buildIndex;
         int nextSceneIndex = currentScene + 1;
 		if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
 		{
+			// Load next level
 			SceneManager.UnloadScene(currentScene);
 			SceneManager.LoadScene(nextSceneIndex, LoadSceneMode.Additive);
+
+			// Respawn player
+			GameObject playerObject = GameObject.FindGameObjectWithTag(PLAYER_TAG_NAME);
+			if (playerObject != null)
+			{
+				playerRespawn = playerObject.GetComponent<PlayerRespawn>();
+				if (playerRespawn)
+				{
+					playerRespawn.Respawn();
+				}
+				else
+				{
+					Debug.Log("No respawn component found on player. Please add PlayerRespawn component to player");
+				}
+			}
+			else
+			{
+				Debug.Log("No player object found");
+			}
 		}
 	}
 }
