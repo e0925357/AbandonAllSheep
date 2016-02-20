894922a27ba5d20325645057306629d11cbca90c
Lukas Frühstück <lukas.fruhstuck@gmail.com>
2016-02-20 19:20:18 +0100

    big spike added

---
+++ Assets/Scripts/BigSpike.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigSpike : MonoBehaviour, SheepKiller
+{
+    public Sprite BloodySprite;
+    public GameObject DeadSheep;
+    public GameObject SpikeSprite;
+    public Animator SpikeAnimator;
+    public float HiddenTime;
+
+    private SpriteRenderer spriteRenderer;
+	void Start ()
+	{
+	    spriteRenderer = SpikeSprite.GetComponent<SpriteRenderer>();
+        InvokeRepeating("CycleAnimation", 0, HiddenTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void CycleAnimation()
+    {
+        SpikeAnimator.SetTrigger("StartMove");
+    }
+
+    public void SheepHit(GameObject sheep)
+    {
+        spriteRenderer.sprite = BloodySprite;
+
+        GameObject deadSheep = Instantiate(DeadSheep);
+        deadSheep.transform.position = sheep.transform.position;
+        deadSheep.transform.parent = transform.parent;
+    }
+
+
+
+    void OnParticleCollision(GameObject other)
+    {
+        spriteRenderer.sprite = BloodySprite;
+    }
+}
---
+++ Assets/Scripts/PhysicsSheep.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsSheep : MonoBehaviour
+{
+
+    private bool isDying;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    isDying = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (!isDying)
+        {
+            Acid acid = collider2D.gameObject.GetComponent<Acid>();
+            if (acid != null)
+            {
+                Destroy(gameObject, 15.0f);
+                isDying = true;
+            }
+        }
+    }
+}
