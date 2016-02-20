eca821a73c75514c70c364bbf2c16ef080ae4216
Lukas Frühstück <lukas.fruhstuck@gmail.com>
2016-02-20 11:35:56 +0100

    created spike prefab

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using Prime31;
 
 public class Health : MonoBehaviour {
 
 	public delegate void lifeChange(GameObject go);
 	public static event lifeChange onBirth;
 	public static event lifeChange onDeath;
-
+    
 	// Use this for initialization
 	void Start () {
 		if(onBirth != null)
@@ -15,11 +17,19 @@ public class Health : MonoBehaviour {
 		}
 	}
 
-	void OnDestroy()
+    void OnDestroy()
 	{
 		if (onDeath != null)
 		{
 			onDeath(gameObject);
 		}
 	}
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.GetComponent<Spike>() != null)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
