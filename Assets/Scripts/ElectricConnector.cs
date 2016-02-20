3a3dfe7243ad8eaea8e484603a6571f4adb1e58b
Lukas Frühstück <lukas.fruhstuck@gmail.com>
2016-02-20 21:31:49 +0100

    electrical wire

---
+++ Assets/Scripts/ElectricConnector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricConnector : MonoBehaviour, SheepKiller
+{
+
+    public GameObject SheepRoot;
+    public GameObject ElectricParticles;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Active = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void SheepHit(GameObject sheep)
+    {
+        GameObject particles = Instantiate(ElectricParticles);
+        particles.transform.position = SheepRoot.transform.position;
+        Destroy(particles, 2.0f);
+    }
+
+    public bool Active { get; private set; }
+}
