2c37aa525f2b68935a216b6646e761444e94fcba
Stefan Zaufl <stefan.zaufl@hotmail.com>
2016-02-20 21:06:49 +0100

    Added title

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : MonoBehaviour {
+
+	public Animator titleAnimator;
+	public Renderer titleRenderer;
+
+	private bool activated = false;
+
+	void OnEnable()
+	{
+		Health.onDeath += onSheepDeath;
+	}
+
+	void OnDisable()
+	{
+		Health.onDeath -= onSheepDeath;
+	}
+
+	void onSheepDeath(GameObject sheep)
+	{
+		if (activated) return;
+
+		activated = true;
+		titleRenderer.enabled = true;
+		titleAnimator.SetTrigger("start");
+	}
+}
