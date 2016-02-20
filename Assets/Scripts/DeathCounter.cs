b03b2ebc0deb9fbb5d6e49fcaf853e683cd23253
Stefan Zaufl <stefan.zaufl@hotmail.com>
2016-02-20 22:52:35 +0100

    First version of the death counter.

---
+++ Assets/Scripts/DeathCounter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DeathCounter : MonoBehaviour {
+
+	public Text deathsText;
+	public Color flashColor;
+	public float flashTime = 1.0f;
+	public float flashScale = 1.5f;
+
+	private Color defaultColor;
+
+	int deathCount = 0;
+
+	void OnEnable()
+	{
+		Health.onDeath += sheepDeath;
+	}
+
+	void OnDisable()
+	{
+		Health.onDeath -= sheepDeath;
+	}
+
+	void sheepDeath(GameObject sheep)
+	{
+		deathCount++;
+
+		deathsText.text = "" + deathCount;
+
+		StartCoroutine(flash());
+	}
+
+	IEnumerator flash()
+	{
+		Vector3 maxScale = new Vector3(flashScale, flashScale, 1);
+		Vector3 minScale = new Vector3(1, 1, 1);
+
+		defaultColor = deathsText.color;
+		deathsText.color = flashColor;
+		deathsText.transform.localScale = maxScale;
+
+		for (float timer = 0; timer < flashTime; timer += Time.deltaTime)
+		{
+			yield return new WaitForEndOfFrame();
+
+			float t = timer / flashTime;
+
+			deathsText.color = Color.Lerp(flashColor, defaultColor, t);
+			deathsText.transform.localScale = Vector3.Lerp(maxScale, minScale, t);
+		}
+
+		deathsText.color = defaultColor;
+		deathsText.transform.localScale = minScale;
+	}
+}
