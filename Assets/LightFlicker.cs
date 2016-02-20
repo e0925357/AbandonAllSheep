f2b3a88382267f281b69c7b733c57821429c05a0
Lukas Frühstück <lukas.fruhstuck@gmail.com>
2016-02-21 00:53:17 +0100

    light flicker

---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private float initialAlpha;
+    private float random;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    spriteRenderer = GetComponent<SpriteRenderer>();
+	    initialAlpha = spriteRenderer.color.a;
+        random = Random.Range(0.0f, 65535.0f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        Color color = spriteRenderer.color;
+        float noise = Mathf.PerlinNoise(random, Time.time);
+        color.a = initialAlpha * noise;
+        spriteRenderer.color = color;
+	}
+}
