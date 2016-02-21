42bddfe5aaedac9d84690301890775fb0bc90877
Lukas Frühstück <lukas.fruhstuck@gmail.com>
2016-02-21 09:38:42 +0100

    light flicker fixed

--- Assets/Scripts/LightFlicker.cs
+++ Assets/Scripts/LightFlicker.cs
@@ -4,14 +4,12 @@ using System.Collections;
 public class LightFlicker : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-    private float initialAlpha;
     private float random;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    spriteRenderer = GetComponent<SpriteRenderer>();
-	    initialAlpha = spriteRenderer.color.a;
         random = Random.Range(0.0f, 65535.0f);
     }
 	
@@ -20,7 +18,7 @@ public class LightFlicker : MonoBehaviour
 	{
         Color color = spriteRenderer.color;
         float noise = Mathf.PerlinNoise(random, Time.time);
-        color.a = initialAlpha * noise;
+        color.a = Mathf.Lerp(0.1f, 0.3f, noise); 
         spriteRenderer.color = color;
 	}
 }
