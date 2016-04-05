2dc9f06c87508fbee88ead5636fdef3369dfc0c7
David Portisch <david.portisch@outlook.com>
2016-04-05 13:24:15 +0200

    Fixed error messages in main menu

---
+++ Assets/Scripts/MenuLevelSelect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuLevelSelect : MonoBehaviour {
+
+	public void LoadLevel(string levelName)
+	{
+		if (!string.IsNullOrEmpty(levelName))
+		{
+			SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+		}
+		else
+		{
+			Debug.Log("No level name given for loading level");
+		}
+	}
+}
