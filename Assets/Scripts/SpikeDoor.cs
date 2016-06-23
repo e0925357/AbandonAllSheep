f139b990ce3f6655b17af81825678d74ac18ac8d
Stefan Zaufl <stefan.zaufl@hotmail.com>
2016-06-23 18:24:28 +0200

    Spike doors are now more consistent & less deadly.

--- Assets/Scripts/SpikeDoor.cs
+++ Assets/Scripts/SpikeDoor.cs
@@ -3,59 +3,66 @@ using System.Collections;
 
 public class SpikeDoor : MonoBehaviour, SheepKiller
 {
+	public float doorDeathDelay = 0.5f;
 
-    private Animator animator;
-    private BoxCollider2D collider2d;
-    private bool isClosing;
+	private Animator animator;
+	private BoxCollider2D collider2d;
+	private volatile bool isClosing;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    animator = GetComponent<Animator>();
-        collider2d = GetComponent<BoxCollider2D>();
-        isClosing = false;
+		animator = GetComponent<Animator>();
+		collider2d = GetComponent<BoxCollider2D>();
+		isClosing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+		
 	}
 
-    public void Open()
-    {
-        animator.SetTrigger("Open");
-        Debug.Log("Open");
-    }
+	public void Open()
+	{
+		animator.SetTrigger("Open");
+		Debug.Log("Open");
+	}
 
 
-    public void Close()
-    {
-        Debug.Log("Close");
-        animator.SetTrigger("Close");
-        collider2d.enabled = true;
-        collider2d.isTrigger = true;
-        gameObject.layer = LayerMask.NameToLayer("Trigger");
-        isClosing = true;
-    }
+	public void Close()
+	{
+		Debug.Log("Close");
+		animator.SetTrigger("Close");
 
-    public void ClosingFinished()
-    {
-        Debug.Log("ClosingFinished");
-        isClosing = false;
-        collider2d.isTrigger = false;
-        gameObject.layer = LayerMask.NameToLayer("Default");
-    }
+		Invoke("beginDeadlyPhase", doorDeathDelay);
+	}
 
-    public void OpeningFinished()
-    {
-        Debug.Log("OpeningFinished");
-        collider2d.enabled = false;
-    }
+	private void beginDeadlyPhase()
+	{
+		collider2d.enabled = true;
+		collider2d.isTrigger = true;
+		gameObject.layer = LayerMask.NameToLayer("Trigger");
+		isClosing = true;
+	}
 
-    public AudioClip SheepHit(GameObject sheep)
-    {
+	public void ClosingFinished()
+	{
+		Debug.Log("ClosingFinished");
+		isClosing = false;
+		collider2d.isTrigger = false;
+		gameObject.layer = LayerMask.NameToLayer("Default");
+	}
+
+	public void OpeningFinished()
+	{
+		Debug.Log("OpeningFinished");
+		collider2d.enabled = false;
+	}
+
+	public AudioClip SheepHit(GameObject sheep)
+	{
 		return null;
-    }
+	}
 
-    public bool Active { get { return isClosing; } }
+	public bool Active { get { return isClosing; } }
 }
