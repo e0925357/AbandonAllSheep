d614c6b1632d81ac3cc9def8ceb4691c26018492
David Portisch <david.portisch@outlook.com>
2016-07-20 19:19:44 +0200

    Implemented rest calls to the Global sheep counter API into the game --> Global sheep counter works now; Implemented RestFactory which is capable of making POST and GET requests

---
+++ Assets/Scripts/Backend/RestFactory.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Simple RestFactory to make GET and POST Requests.
+/// </summary>
+public class RestFactory : MonoBehaviour
+{
+	private string results;
+
+	/// <summary>
+	/// The result of the last completed request. null, if there was not request yet or the last requets failed.
+	/// </summary>
+	public string Results
+	{
+		get { return results; }
+	}
+
+	/// <summary>
+	/// Makes a Post Request which is executed in a coroutine. When the request has completed, onComplete ist called.
+	/// </summary>
+	/// <param name="url">The url of the POST request. The url must be '%' escaped.</param>
+	/// <param name="wwwForm">The wwwForm to use the parameters from. Must not be null.</param>
+	/// <param name="onComplete">Called when the request hast completed (even if it has failed --> check the www.error field)</param>
+	/// <returns>The WWW object used for the request.</returns>
+	public WWW RestPost(string url, WWWForm wwwForm, Action<WWW> onComplete)
+	{
+		if (wwwForm == null)
+		{
+			throw new ArgumentNullException("wwwForm");
+		}
+
+		WWW www = new WWW(url, wwwForm);
+		StartCoroutine(WaitForRequest(www, onComplete));
+		return www;
+	}
+
+	/// <summary>
+	/// Makes a Post Request which is executed in a coroutine. When the request has completed, onComplete ist called.
+	/// This method allows custom data and headers to be used by the request.
+	/// </summary>
+	/// <param name="url">The url of the POST request. The url must be '%' escaped.</param>
+	/// <param name="postData">The data to be posted.</param>
+	/// <param name="headers">The headers to be used.</param>
+	/// <param name="onComplete">Called when the request hast completed (even if it has failed --> check the www.error field)</param>
+	/// <returns>The WWW object used for the request.</returns>
+	public WWW RestPost(string url, byte[] postData, Dictionary<string, string> headers, Action<WWW> onComplete)
+	{
+		WWW www = new WWW(url, postData, headers);
+		StartCoroutine(WaitForRequest(www, onComplete));
+		return www;
+	}
+
+	/// <summary>
+	/// Creates a Get request which is executed in a coroutine. When the request has completed, onComplete ist called.
+	/// </summary>
+	/// <param name="url">The url of the GET request. The url must be '%' escaped.</param>
+	/// <param name="onComplete">Called when the request hast completed (even if it has failed --> check the www.error field)</param>
+	/// <returns>The WWW object used for the request.</returns>
+	public WWW RestGet(string url, Action<WWW> onComplete)
+	{
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www, onComplete));
+		return www;
+	}
+
+	/// <summary>
+	/// Method which waits for the result of the www object. Executed as Coroutine.
+	/// </summary>
+	/// <param name="www">The WWW object to wait for.</param>
+	/// <param name="onComplete">Called when the request hast completed (even if it has failed --> check the www.error field)</param>
+	/// <returns>The IEnumerator for the Coroutine mechanism.</returns>
+	private IEnumerator WaitForRequest(WWW www, Action<WWW> onComplete)
+	{
+		yield return www;
+
+		if (www.error == null)
+		{
+			results = www.text;
+		}
+		if (www.error != null)
+		{
+			results = null;
+			Debug.Log(string.Format("Rest Request failed: {0}", www.error));
+		}
+		onComplete(www);
+	}
+}
---
+++ Assets/Scripts/Backend/SheepKillRestNotifier.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Notifier for the BloodySheep Backend to report a killed sheep.
+/// Uses the BloodySheep Rest-API.
+/// </summary>
+[RequireComponent(typeof(RestFactory))]
+public class SheepKillRestNotifier : MonoBehaviour
+{
+	private RestFactory restFactory;
+
+	[Tooltip("The url to be called by the post request.")]
+	public string url = "https://api.bloodysheep.lukasf.at/sheep/kill";
+
+	// Use this for initialization
+	void Start()
+	{
+		restFactory = GetComponent<RestFactory>();
+	}
+
+	void OnEnable()
+	{
+		Health.onDeath += SheepDeath;
+	}
+
+	void OnDisable()
+	{
+		Health.onDeath -= SheepDeath;
+	}
+
+	/// <summary>
+	/// Callback method which is registered at the Health script. Only use for this purpose.
+	/// To notify the Backend about a kill use SheepKilled() instead.
+	/// </summary>
+	/// <param name="sheep">The killed sheep gameobject.</param>
+	void SheepDeath(GameObject sheep)
+	{
+		SheepKilled();
+	}
+
+	/// <summary>
+	/// Called whenever a sheep died.
+	/// </summary>
+	public void SheepKilled()
+	{
+		WWWForm wwwForm = new WWWForm();
+
+		// Add some dummy data, because rest POST request with a zero sized post buffer is not supported
+		wwwForm.AddField("Sheep", "Maeh");
+		byte[] rawData = wwwForm.data;
+		Dictionary<string, string> headers = wwwForm.headers;
+
+		// Set the current user in the Authentication to uniquely identify users
+		// TODO: Currently it uses the device id --> use real User Names later instead
+		headers.Add("Authorization", SystemInfo.deviceUniqueIdentifier);
+
+		restFactory.RestPost(url, rawData, headers, KillRestCompleted);
+	}
+
+	/// <summary>
+	/// Called when the rest request for the sheep kill has completed.
+	/// </summary>
+	/// <param name="www">The WWW object which was used for the request.</param>
+	private void KillRestCompleted(WWW www)
+	{
+		// Do nothing for the moment
+	}
+}
